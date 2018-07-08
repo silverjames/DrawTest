e7ba0e6b9b68a9e4c76a57da4b8b7f4642509cd3
Bernhard F. Kraft <bernhard@bfk-engineering.com>
2018-07-08 19:59:50 +0200

    Initial Commit

---
+++ DrawTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrawTest
+//
+//  Created by Bernhard F. Kraft on 08.07.18.
+//  Copyright © 2018 Bernhard F. Kraft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
